2b0c07485c5859b79b4448a3d4132dd78059bb8f
Elton Rodrigues <eltonrms.leite@hotmail.com>
2019-01-20 13:04:08 -0200

    Feito ajustes no layout.

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -3,33 +3,36 @@
     <div class="login-background"></div>
     <section class="login-form">
       <img
-        class="login-form__img"
-        src="../../assets/images/logo.png"
-        alt="Logo do parcele sua faculdade">
+          class="login-form__img"
+          src="../../assets/images/logo.png"
+          alt="Logo do parcele sua faculdade">
       <form class="login-form__form" novalidate autocomplete="off">
         <div class="input-group">
           <input
-            class="input-group__field"
-            type="text"
-            id="login"
-            maxlength="255"
-            :class="{ 'input-group__field--active': login }"
-            v-model="login">
+              class="input-group__field"
+              type="text"
+              id="login"
+              maxlength="255"
+              :class="{ 'input-group__field--active': login }"
+              v-model="login">
           <label class="input-group__label" for="login">Login</label>
         </div>
         <div class="input-group">
           <input
-            class="input-group__field"
-            type="password"
-            id="password"
-            maxlength="255"
-            :class="{ 'input-group__field--active': password }"
-            v-model="password">
+              class="input-group__field"
+              type="password"
+              id="password"
+              maxlength="255"
+              :class="{ 'input-group__field--active': password }"
+              v-model="password">
           <label class="input-group__label" for="password">Senha</label>
           <i class="input-group__icon icon-close"></i>
         </div>
         <div class="login-form__btns">
-          <a class="login-form__forgot-password" href="#">Esqueceu a senha?</a>
+          <router-link
+              class="login-form__forgot-password"
+              to="/esqueceu-sua-senha">Esqueceu a senha?
+          </router-link>
           <button class="btn btn--login">Login</button>
         </div>
       </form>
