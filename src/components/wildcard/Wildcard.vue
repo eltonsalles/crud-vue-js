5e4fce2a9692bf6a64f06bc102197a971bf3937a
Elton Rodrigues <eltonrms.leite@hotmail.com>
2019-02-18 23:24:25 -0300

    Criado o wildcard e ajustado partes do layout.

---
+++ src/components/wildcard/Wildcard.vue
@@ -0,0 +1,18 @@
+<template>
+  <ul class="wildcard">
+    <li class="wildcard__item wildcard__item--active">Dados da empresa</li>
+    <li class="wildcard__item">Campus</li>
+    <li class="wildcard__item">Cursos</li>
+    <li class="wildcard__item">Layout</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'Wildcard',
+};
+</script>
+
+<style lang="postcss" scoped>
+  @import "./Wildcard.css";
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,17 +1,22 @@
 <template>
   <div>
     <m-breadcrumbs></m-breadcrumbs>
+    <div class="home">
+      <m-wildcard></m-wildcard>
+    </div>
   </div>
 </template>
 
 <script>
 import Breadcrumbs from '../../components/breadcrumbs/Breadcrumbs.vue';
+import Wildcard from '../../components/wildcard/Wildcard.vue';
 
 export default {
   name: 'Home',
 
   components: {
     'm-breadcrumbs': Breadcrumbs,
+    'm-wildcard': Wildcard,
   },
 };
 </script>
