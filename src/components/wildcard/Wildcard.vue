acb51cbd5c79a7d5bbf844d9518afcec6b5937ee
Elton Rodrigues <eltonrms.leite@hotmail.com>
2019-03-04 16:47:19 -0300

    Criado um propriedade para destacar o wildcard ativo e os anteriores.

--- src/components/wildcard/Wildcard.vue
+++ src/components/wildcard/Wildcard.vue
@@ -1,15 +1,36 @@
 <template>
   <ul class="wildcard">
-    <li class="wildcard__item wildcard__item--active">Dados da empresa</li>
-    <li class="wildcard__item">Campus</li>
-    <li class="wildcard__item">Cursos</li>
-    <li class="wildcard__item">Layout</li>
+    <li class="wildcard__item" :class="setWildcardActive(0)">Dados da empresa</li>
+    <li class="wildcard__item" :class="setWildcardActive(1)">Campus</li>
+    <li class="wildcard__item" :class="setWildcardActive(2)">Cursos</li>
+    <li class="wildcard__item" :class="setWildcardActive(3)">Layout</li>
   </ul>
 </template>
 
 <script>
 export default {
   name: 'Wildcard',
+
+  props: {
+    wildcardActive: {
+      type: Number,
+      required: true,
+    },
+  },
+
+  methods: {
+    setWildcardActive(value) {
+      if (value < this.wildcardActive) {
+        return 'wildcard__item--previous';
+      }
+
+      if (value === this.wildcardActive) {
+        return 'wildcard__item--active';
+      }
+
+      return '';
+    },
+  },
 };
 </script>
 
