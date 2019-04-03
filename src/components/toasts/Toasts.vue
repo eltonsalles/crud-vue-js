21f87666c669b876654c9d81e4aebff98f5c29b8
Elton Rodrigues <eltonrms.leite@hotmail.com>
2019-04-02 21:23:28 -0300

    Colocado as cores de sucesso, info, aviso e erro no arquivo base.css e criado o componente para mostrar as mensagens.

---
+++ src/components/toasts/Toasts.vue
@@ -0,0 +1,47 @@
+<template>
+  <transition name="fade">
+    <div class="c-toasts" v-if="isShow">
+      <div class="c-toasts__timer"></div>
+      <div class="c-toasts__content">
+        <div>
+          <slot></slot>
+        </div>
+        <button class="c-btn" @click="$emit('closeToasts')">
+          <i class="material-icons">close</i>
+        </button>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: 'Toasts',
+
+  props: {
+    isShow: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+
+  methods: {
+    closeToasts() {
+      setTimeout(() => {
+        this.$emit('closeToasts');
+      }, 5000);
+    },
+  },
+
+  watch: {
+    isShow(val) {
+      if (val) {
+        this.closeToasts();
+      }
+    },
+  },
+};
+</script>
+
+<style scoped src="./Toasts.css"></style>
