3737869340e5f2b4595381db3e700503253576b1
Elton Rodrigues <eltonrms.leite@hotmail.com>
2019-04-06 20:30:58 -0300

    Arrumado a estrutura html.

--- src/views/college/new/company-data/CompanyData.vue
+++ src/views/college/new/company-data/CompanyData.vue
@@ -1,203 +1,205 @@
 <template>
-  <form class="page-content" novalidate autocomplete="off">
-    <section class="page-content__left-part">
-      <h4 class="page-content__title">Dados da empresa</h4>
-      <div
-          class="input-group"
-          :class="{ 'input-group--error': $v.companyData.fantasyName.$error }">
-        <input
-            class="input-group__field"
-            type="text"
-            id="fantasy-name"
-            maxlength="255"
-            :class="{ 'input-group__field--active': companyData.fantasyName }"
-            v-model="companyData.fantasyName">
-        <label class="input-group__label" for="fantasy-name">Nome Fantasia</label>
-      </div>
-      <div
-          class="input-group"
-          :class="{ 'input-group--error': $v.companyData.socialName.$error }">
-        <input
-            class="input-group__field"
-            type="text"
-            id="social-name"
-            maxlength="255"
-            :class="{ 'input-group__field--active': companyData.socialName }"
-            v-model="companyData.socialName">
-        <label class="input-group__label" for="social-name">Razão Social</label>
-      </div>
-      <div
-          class="input-group"
-          :class="{ 'input-group--error': $v.companyData.cnpj.$error }">
-        <input
-            class="input-group__field"
-            type="text"
-            id="cnpj"
-            maxlength="18"
-            v-mask="'##.###.###/####-##'"
-            :class="{ 'input-group__field--active': companyData.cnpj }"
-            v-model="companyData.cnpj">
-        <label class="input-group__label" for="cnpj">CNPJ</label>
-      </div>
-      <div
-          class="input-group"
-          :class="{ 'input-group--error': $v.companyData.stateRegistration.$error }">
-        <input
-            class="input-group__field"
-            type="text"
-            id="state-registration"
-            maxlength="255"
-            v-mask="'###.###.###.###'"
-            :class="{ 'input-group__field--active': companyData.stateRegistration }"
-            v-model="companyData.stateRegistration">
-        <label class="input-group__label" for="state-registration">Inscrição Estadual</label>
-      </div>
-      <div
-          class="input-group"
-          :class="{ 'input-group--error': $v.companyData.municipalRegistration.$error }">
-        <input
-            class="input-group__field"
-            type="text"
-            id="municipal-registration"
-            maxlength="255"
-            v-mask="'###.###.###.###'"
-            :class="{ 'input-group__field--active': companyData.municipalRegistration }"
-            v-model="companyData.municipalRegistration">
-        <label
-            class="input-group__label" for="municipal-registration">Inscrição Municipal</label>
-      </div>
-      <m-select
-          :class="{ 'select-group--error': $v.companyData.financialOfficer.$error }"
-          label="Responsável Financeiro"
-          :options="financialOfficer.options"
-          v-model="companyData.financialOfficer"></m-select>
-    </section>
-    <section class="page-content__right-part">
-      <h4 class="page-content__title">Endereço</h4>
-      <div
-          class="input-group"
-          :class="{ 'input-group--error': $v.companyData.address.cep.$error }">
-        <input
-            class="input-group__field"
-            type="text"
-            id="cep"
-            maxlength="9"
-            v-mask="'#####-###'"
-            :class="{ 'input-group__field--active': companyData.address.cep }"
-            v-model="companyData.address.cep">
-        <label class="input-group__label" for="cep">CEP</label>
-      </div>
-      <div class="form-group">
-        <div
-            class="input-group input-group--size-3"
-            :class="{ 'input-group--error': $v.companyData.address.address.$error }">
-          <input
-              class="input-group__field"
-              type="text"
-              id="address"
-              maxlength="255"
-              :class="{ 'input-group__field--active': companyData.address.address }"
-              v-model="companyData.address.address">
-          <label class="input-group__label" for="address">Endereço</label>
-        </div>
-        <div
-            class="input-group input-group--size-1"
-            :class="{ 'input-group--error': $v.companyData.address.number.$error }">
-          <input
-              class="input-group__field"
-              type="text"
-              id="number"
-              maxlength="255"
-              :class="{ 'input-group__field--active': companyData.address.number }"
-              v-model="companyData.address.number">
-          <label class="input-group__label" for="number">Número</label>
-        </div>
-      </div>
-      <div class="form-group">
+  <div>
+    <form class="page-content" novalidate autocomplete="off">
+      <section class="page-content__left-part">
+        <h4 class="page-content__title">Dados da empresa</h4>
         <div
             class="input-group"
-            :class="{ 'input-group--error': $v.companyData.address.complement.$error }">
+            :class="{ 'input-group--error': $v.companyData.fantasyName.$error }">
           <input
               class="input-group__field"
               type="text"
-              id="complement"
+              id="fantasy-name"
               maxlength="255"
-              :class="{ 'input-group__field--active': companyData.address.complement }"
-              v-model="companyData.address.complement">
-          <label class="input-group__label" for="complement">Complemento</label>
+              :class="{ 'input-group__field--active': companyData.fantasyName }"
+              v-model="companyData.fantasyName">
+          <label class="input-group__label" for="fantasy-name">Nome Fantasia</label>
         </div>
         <div
             class="input-group"
-            :class="{ 'input-group--error': $v.companyData.address.district.$error }">
+            :class="{ 'input-group--error': $v.companyData.socialName.$error }">
           <input
               class="input-group__field"
               type="text"
-              id="district"
+              id="social-name"
               maxlength="255"
-              :class="{ 'input-group__field--active': companyData.address.district }"
-              v-model="companyData.address.district">
-          <label class="input-group__label" for="district">Bairro</label>
-        </div>
-      </div>
-      <div class="form-group">
-        <div
-            class="input-group"
-            :class="{ 'input-group--error': $v.companyData.address.city.$error }">
-          <input
-              class="input-group__field"
-              type="text"
-              id="city"
-              maxlength="255"
-              :class="{ 'input-group__field--active': companyData.address.city }"
-              v-model="companyData.address.city">
-          <label class="input-group__label" for="city">Cidade</label>
+              :class="{ 'input-group__field--active': companyData.socialName }"
+              v-model="companyData.socialName">
+          <label class="input-group__label" for="social-name">Razão Social</label>
         </div>
         <div
             class="input-group"
-            :class="{ 'input-group--error': $v.companyData.address.state.$error }">
+            :class="{ 'input-group--error': $v.companyData.cnpj.$error }">
           <input
               class="input-group__field"
               type="text"
-              id="state"
-              maxlength="255"
-              :class="{ 'input-group__field--active': companyData.address.state }"
-              v-model="companyData.address.state">
-          <label class="input-group__label" for="state">Estado</label>
+              id="cnpj"
+              maxlength="18"
+              v-mask="'##.###.###/####-##'"
+              :class="{ 'input-group__field--active': companyData.cnpj }"
+              v-model="companyData.cnpj">
+          <label class="input-group__label" for="cnpj">CNPJ</label>
         </div>
-      </div>
-      <section>
-        <h4 class="page-content__title page-content__title--not-margin">Selecionar produto</h4>
+        <div
+            class="input-group"
+            :class="{ 'input-group--error': $v.companyData.stateRegistration.$error }">
+          <input
+              class="input-group__field"
+              type="text"
+              id="state-registration"
+              maxlength="255"
+              v-mask="'###.###.###.###'"
+              :class="{ 'input-group__field--active': companyData.stateRegistration }"
+              v-model="companyData.stateRegistration">
+          <label class="input-group__label" for="state-registration">Inscrição Estadual</label>
+        </div>
+        <div
+            class="input-group"
+            :class="{ 'input-group--error': $v.companyData.municipalRegistration.$error }">
+          <input
+              class="input-group__field"
+              type="text"
+              id="municipal-registration"
+              maxlength="255"
+              v-mask="'###.###.###.###'"
+              :class="{ 'input-group__field--active': companyData.municipalRegistration }"
+              v-model="companyData.municipalRegistration">
+          <label
+              class="input-group__label" for="municipal-registration">Inscrição Municipal</label>
+        </div>
+        <m-select
+            :class="{ 'select-group--error': $v.companyData.financialOfficer.$error }"
+            label="Responsável Financeiro"
+            :options="financialOfficer.options"
+            v-model="companyData.financialOfficer"></m-select>
+      </section>
+      <section class="page-content__right-part">
+        <h4 class="page-content__title">Endereço</h4>
+        <div
+            class="input-group"
+            :class="{ 'input-group--error': $v.companyData.address.cep.$error }">
+          <input
+              class="input-group__field"
+              type="text"
+              id="cep"
+              maxlength="9"
+              v-mask="'#####-###'"
+              :class="{ 'input-group__field--active': companyData.address.cep }"
+              v-model="companyData.address.cep">
+          <label class="input-group__label" for="cep">CEP</label>
+        </div>
         <div class="form-group">
-          <m-checkbox
-              :class="{ 'checkbox-group--error': $v.companyData.products.$error }"
-              @input="checkedProduct"
-              label="Sem juros"></m-checkbox>
-          <m-checkbox
-              :class="{ 'checkbox-group--error': $v.companyData.products.$error }"
-              @input="checkedProduct"
-              label="Gestão"></m-checkbox>
-          <m-checkbox
-              :class="{ 'checkbox-group--error': $v.companyData.products.$error }"
-              @input="checkedProduct"
-              label="Financie 50%"></m-checkbox>
-          <m-checkbox
-              :class="{ 'checkbox-group--error': $v.companyData.products.$error }"
-              @input="checkedProduct"
-              label="Financie 25%"></m-checkbox>
+          <div
+              class="input-group input-group--size-3"
+              :class="{ 'input-group--error': $v.companyData.address.address.$error }">
+            <input
+                class="input-group__field"
+                type="text"
+                id="address"
+                maxlength="255"
+                :class="{ 'input-group__field--active': companyData.address.address }"
+                v-model="companyData.address.address">
+            <label class="input-group__label" for="address">Endereço</label>
+          </div>
+          <div
+              class="input-group input-group--size-1"
+              :class="{ 'input-group--error': $v.companyData.address.number.$error }">
+            <input
+                class="input-group__field"
+                type="text"
+                id="number"
+                maxlength="255"
+                :class="{ 'input-group__field--active': companyData.address.number }"
+                v-model="companyData.address.number">
+            <label class="input-group__label" for="number">Número</label>
+          </div>
         </div>
+        <div class="form-group">
+          <div
+              class="input-group"
+              :class="{ 'input-group--error': $v.companyData.address.complement.$error }">
+            <input
+                class="input-group__field"
+                type="text"
+                id="complement"
+                maxlength="255"
+                :class="{ 'input-group__field--active': companyData.address.complement }"
+                v-model="companyData.address.complement">
+            <label class="input-group__label" for="complement">Complemento</label>
+          </div>
+          <div
+              class="input-group"
+              :class="{ 'input-group--error': $v.companyData.address.district.$error }">
+            <input
+                class="input-group__field"
+                type="text"
+                id="district"
+                maxlength="255"
+                :class="{ 'input-group__field--active': companyData.address.district }"
+                v-model="companyData.address.district">
+            <label class="input-group__label" for="district">Bairro</label>
+          </div>
+        </div>
+        <div class="form-group">
+          <div
+              class="input-group"
+              :class="{ 'input-group--error': $v.companyData.address.city.$error }">
+            <input
+                class="input-group__field"
+                type="text"
+                id="city"
+                maxlength="255"
+                :class="{ 'input-group__field--active': companyData.address.city }"
+                v-model="companyData.address.city">
+            <label class="input-group__label" for="city">Cidade</label>
+          </div>
+          <div
+              class="input-group"
+              :class="{ 'input-group--error': $v.companyData.address.state.$error }">
+            <input
+                class="input-group__field"
+                type="text"
+                id="state"
+                maxlength="255"
+                :class="{ 'input-group__field--active': companyData.address.state }"
+                v-model="companyData.address.state">
+            <label class="input-group__label" for="state">Estado</label>
+          </div>
+        </div>
+        <section>
+          <h4 class="page-content__title page-content__title--not-margin">Selecionar produto</h4>
+          <div class="form-group">
+            <m-checkbox
+                :class="{ 'checkbox-group--error': $v.companyData.products.$error }"
+                @input="checkedProduct"
+                label="Sem juros"></m-checkbox>
+            <m-checkbox
+                :class="{ 'checkbox-group--error': $v.companyData.products.$error }"
+                @input="checkedProduct"
+                label="Gestão"></m-checkbox>
+            <m-checkbox
+                :class="{ 'checkbox-group--error': $v.companyData.products.$error }"
+                @input="checkedProduct"
+                label="Financie 50%"></m-checkbox>
+            <m-checkbox
+                :class="{ 'checkbox-group--error': $v.companyData.products.$error }"
+                @input="checkedProduct"
+                label="Financie 25%"></m-checkbox>
+          </div>
+        </section>
+        <section class="page-content__actions">
+          <button
+              class="btn btn--next"
+              @click.prevent="nextForm">
+            Próximo<i class="icon-right icon-sharp-arrow_forward-24px"></i>
+          </button>
+        </section>
       </section>
-      <section class="page-content__actions">
-        <button
-            class="btn btn--next"
-            @click.prevent="nextForm">
-          Próximo<i class="icon-right icon-sharp-arrow_forward-24px"></i>
-        </button>
-      </section>
-    </section>
+    </form>
     <m-toasts class="toasts--danger" :is-show="isShowToasts" @closeToasts="showToasts = false">
       <p>Existem campos pendentes e/ou não preenchidos.</p>
     </m-toasts>
-  </form>
+  </div>
 </template>
 
 <script>
